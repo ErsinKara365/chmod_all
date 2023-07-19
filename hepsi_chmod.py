3182e81d8d7c08772b90fcf7864bc601dc782c74
Ersin Kara <92259139+ErsinKara365@users.noreply.github.com>
2023-07-19 12:11:08 +0000

    Update hepsi_chmod.py

--- hepsi_chmod.py
+++ hepsi_chmod.py
@@ -2,32 +2,15 @@ import os
 import time
 import subprocess as sp
 
-
-
-
-for anadizin, klasor, dosya  in os.walk(os.getcwd()):
-	
+for anadizin, klasor, dosya  in os.walk(os.getcwd()):	
 	for ata in dosya:
 		metin = anadizin +"/"+ ata		
 		sp.run(["chmod","755",metin])
 		sp.run(["ls","-l",anadizin])
-		time.sleep(.5)
+		time.sleep(.2)
 
 	for bir in klasor:
 		metin = anadizin +"/"+ bir		
 		sp.run(["chmod","755",metin])
 		sp.run(["ls","-l",anadizin])
-		time.sleep(.5)
-        
-            
-        
-    
-        
-    
-
-
-# for ata in os.scandir(os.getcwd()):
-#     if ata == "hepsi_chmod.py" :
-#         continue
-#     print(os.stat(ata).st_size)
-	
+		time.sleep(.2)
