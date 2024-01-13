b1459dc4a92a75bd61f085a669ffb67300792789
Ersin Kara <92259139+ErsinKara365@users.noreply.github.com>
2024-01-13 21:51:14 +0300

    os.walk.py

--- hepsi_chmod.py
+++ hepsi_chmod.py
@@ -14,3 +14,28 @@ for anadizin, klasor, dosya  in os.walk(os.getcwd()):
 		sp.run(["chmod","755",metin])
 		sp.run(["ls","-l",anadizin])
 		time.sleep(.2)
+
+
+# Bütün mp4 uzantiları cekme
+#not -> klasorleri 01-02-03... seklinde sırala 
+
+import os
+import time
+import subprocess as sp
+
+dizinYolu = "/home/ersin/Desktop/dersler/tasinacak_klasor"
+sayi = 0 
+
+yol = os.listdir(os.getcwd())
+yol.sort()
+
+for anadizin, klasorler, dosyalar in os.walk(os.getcwd()):
+	klasorler.sort()
+	
+	for dosya in dosyalar:
+		if dosya.endswith(".mp4"):
+			veri = anadizin+"/"+dosya
+			Yol = dizinYolu + str(sayi)
+			sp.run(["cp", veri ,Yol])
+			sayi+=1
+			time.sleep(.2)
